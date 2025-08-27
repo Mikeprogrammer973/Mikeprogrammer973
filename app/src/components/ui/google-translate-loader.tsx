763fee77e4136bd6544e4a53604d59f282ef238c
Mike D. Pascal <96785933+Mikeprogrammer973@users.noreply.github.com>
2025-08-27 12:43:28 -0400

    Update google-translate-loader.tsx

--- app/src/components/ui/google-translate-loader.tsx
+++ app/src/components/ui/google-translate-loader.tsx
@@ -40,7 +40,7 @@ declare global {
     googleTranslateElementInit?: () => void;
     google?: {
       translate: {
-        TranslateElement: new (options: unknown, element: string) => unknown;
+        TranslateElement: new (options: {}, element: string) => unknown;
         TranslateElement: {
           InlineLayout: {
             SIMPLE: number;
