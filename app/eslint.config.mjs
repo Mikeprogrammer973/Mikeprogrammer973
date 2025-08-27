5b3df544a94353f4ed72fabe410bc877b13b3722
Mike D. Pascal <96785933+Mikeprogrammer973@users.noreply.github.com>
2025-08-27 13:30:21 -0400

    Update eslint.config.mjs

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -12,12 +12,6 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
-
-    rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-ts-comment": "off"
-  },
     ignores: [
       "node_modules/**",
       ".next/**",
