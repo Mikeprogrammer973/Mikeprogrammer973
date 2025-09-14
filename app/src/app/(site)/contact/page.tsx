32c824308237adadae8b96281480dc163c184cf3
Mike D. Pascal <96785933+Mikeprogrammer973@users.noreply.github.com>
2025-09-14 19:46:25 -0400

    Update page.tsx

--- app/src/app/(site)/contact/page.tsx
+++ app/src/app/(site)/contact/page.tsx
@@ -78,7 +78,7 @@ export default function ContactPage() {
         )
       } catch (error) {
         console.error('Error sending message:', error)
-        alert('Erro ao enviar mensagem. Tente novamente.')
+        alert('Erro ao enviar mensagem. Tente novamente. 》》' + error)
       } finally {
         setIsSubmitting(false)
       }
